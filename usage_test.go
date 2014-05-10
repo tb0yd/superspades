d089b457dda269964926c113efccd1143d4d2d27
Tyler Boyd <tboyd@rentpath.com>
2014-05-10 19:35:29 -0400

    add another game to usage_test

--- usage_test.go
+++ usage_test.go
@@ -16,17 +16,33 @@ func displayGame(g Game) string {
 }
 
 func TestUsage(t *testing.T) {
-    rand.Seed(123)
+    var err error
+
+    playMove := func(g Game,  m int) Game {
+        g, err = g.Play(m)
+        if err != nil {
+            t.Errorf("%s\n", err.Error())
+        }
+        return g
+    }
+
+    playAllMoves := func(g Game, moves []int) Game {
+        for _, move := range moves {
+            g = playMove(g, move)
+        }
+        return g
+    }
 
     s1 := Score{}
     s2 := Score{}
 
+    rand.Seed(123)
     g := Game{}.Deal()
 
     //bidding round
     g.CurrentPlayer = 1
 
-    g, err := g.Bid(5)
+    g, err = g.Bid(5)
     if err != nil {
         t.Errorf("%s\n", err.Error())
     }
@@ -39,271 +55,70 @@ func TestUsage(t *testing.T) {
     //playing round
     g.CurrentPlayer = 1
 
-    g, err = g.Play(8)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(2)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(4)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(7)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(6)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(3)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(7)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(2)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(6)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(7)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(1)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(1)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(3)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(1)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(5)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(4)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(4)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(3)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(4)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(1)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(3)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(2)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(2)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(2)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(1)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
-
-    g, err = g.Play(0)
-    if err != nil {
-        t.Errorf("%s\n", err.Error())
-    }
+    g = playAllMoves(g, []int{
+        8,0,0,0,
+        2,0,0,4,
+        7,6,3,0,
+        7,2,6,7,
+        1,0,1,0,
+        3,0,0,1,
+        5,0,4,0,
+        0,0,0,4,
+        3,4,1,3,
+        0,2,0,0,
+        2,0,2,0,
+        0,1,0,0,
+        0,0,0,0,
+    })
 
     s1 = s1.Add(g.Books[0], g.Bids[0], g.Books[2], g.Bids[2])
     s2 = s2.Add(g.Books[1], g.Bids[1], g.Books[3], g.Bids[3])
 
     if s1.ToInt() != -54 || s2.ToInt() != 140 {
+        t.Errorf("expected -54 to 140, was %d to %d\n", s1.ToInt(), s2.ToInt())
+    }
+
+    rand.Seed(124)
+    g = Game{}.Deal()
+    g.CurrentPlayer = 2
+
+    g, err = g.Bid(14)
+    if err != nil {
+        t.Errorf("%s\n", err.Error())
+    }
+
+    g, err = g.Bid(4)
+    if err != nil {
+        t.Errorf("%s\n", err.Error())
+    }
+
+    g, err = g.Bid(6)
+    if err != nil {
+        t.Errorf("%s\n", err.Error())
+    }
+    g.CurrentPlayer = 2
+
+    g = playAllMoves(g, []int{
+        4,4,4,0,
+        0,3,3,0,
+        3,3,0,3,
+        6,0,2,5,
+        5,0,1,4,
+        1,3,2,2,
+        6,3,0,2,
+        4,2,2,5,
+        4,1,4,0,
+        3,2,1,1,
+        2,0,2,2,
+        1,1,1,0,
+        0,0,0,0,
+    })
+
+    s1 = s1.Add(g.Books[0], g.Bids[0], g.Books[2], g.Bids[2])
+    s2 = s2.Add(g.Books[1], g.Bids[1], g.Books[3], g.Bids[3])
+
+
+    if s1.ToInt() != -117 || s2.ToInt() != 300 {
         t.Errorf("expected -54 to 140, was %d to %d\n", s1.ToInt(), s2.ToInt())
     }
 }
